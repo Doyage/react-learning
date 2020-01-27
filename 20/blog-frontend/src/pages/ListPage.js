8fd8e97bd35542aac85c520f1adac941c9afad35
Lee <pcstt@naver.com>
2020-01-27 23:48:29 +0900

    add Pagination

--- 20/blog-frontend/src/pages/ListPage.js
+++ 20/blog-frontend/src/pages/ListPage.js
@@ -1,11 +1,17 @@
 import React from "react";
 import PageTemplate from "components/common/PageTemplate";
+import ListWrapper from "components/list/ListWrapper";
+import PostList from "components/list/PostList";
+import Pagintion from "components/list/Pagination";
 
 const ListPage = () => {
 	return (
 		<div>
 			<PageTemplate>
-				List
+				<ListWrapper>
+					<PostList/>
+					<Pagintion/>
+				</ListWrapper>
 			</PageTemplate>
 		</div>
 	);
